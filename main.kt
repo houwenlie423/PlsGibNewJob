aa08b8ff17cee24e9b907ddbb8fc1c47872e47d1
Houwen Lie <houwenlie98@gmail.com>
2024-07-02 20:53:49 +0700

    Solve contains duplicate

---
+++ main.kt
@@ -0,0 +1,11 @@
+import array_and_hashing.containsDuplicate
+
+/**
+ * @author Houwen Lie (houwenlie98@gmail.com)
+ * @version main, v 0.1 Tue 7/2/2024 8:37 PM by Houwen Lie
+ */
+fun main() {
+    println(containsDuplicate(intArrayOf(1,2,3,1)))
+    println(containsDuplicate(intArrayOf(1,2,3,4)))
+    println(containsDuplicate(intArrayOf(1,2,3,3)))
+}
