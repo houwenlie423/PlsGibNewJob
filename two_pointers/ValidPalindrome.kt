62c9679a07a8565491d2c2803007ed3f0b024008
Houwen Lie <houwenlie98@gmail.com>
2024-07-03 19:51:36 +0700

    Solve ValidPalindrome

---
+++ two_pointers/ValidPalindrome.kt
@@ -0,0 +1,36 @@
+package two_pointers
+
+
+/**
+ * @author Houwen Lie (houwenlie98@gmail.com)
+ * @version ValidPalindrome, v 0.1 Wed 7/3/2024 7:40 PM by Houwen Lie
+ * @problem Valid Palindrome - https://leetcode.com/problems/valid-palindrome
+ */
+object ValidPalindrome {
+
+    operator fun invoke(s: String): Boolean {
+        if (s.isBlank()) return true
+
+        var (left, right) = Pair(0, s.lastIndex)
+        while (left <= right) {
+            if (!s[left].isLetterOrDigit()) {
+                left++
+                continue
+            }
+
+            if (!s[right].isLetterOrDigit()) {
+                right--
+                continue
+            }
+
+            if (s[left].lowercase() != s[right].lowercase()) {
+                return false
+            }
+
+            left++
+            right--
+        }
+
+        return true
+    }
+}
